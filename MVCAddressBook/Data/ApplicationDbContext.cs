9ac68f910e3942f5c582ffc5a0bd2a9b02007952
nddarnell_Desktop <darnell.nathaniel95@gmail.com>
2022-05-02 21:18:17 -0700

    Initial commit

---
+++ MVCAddressBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCAddressBook.Models;
+
+namespace MVCAddressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MVCAddressBook
+ Address Book using MVC and C++ .NetASP
